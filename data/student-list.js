2564faf4f5db6443f52d642855fad8c282b31e7f
Matthew Pourroy <pourroy.matt@gmail.com>
2022-06-13 19:46:11 -0400

    student list

---
+++ data/student-list.js
@@ -0,0 +1,9 @@
+const students = [
+    {text: 'Nicholas Hudson', _id: 12345},
+    {text: 'Maria Gomez', _id:23456},
+    {text: 'Javeon Washington', _id:34567},
+  ]
+  
+  export { 
+      students
+  }
